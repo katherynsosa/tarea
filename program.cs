51353a48354ca384e2d962ec042b6c4481f160ef
katherynsosa <48264468+katherynsosa@users.noreply.github.com>
2019-03-05 18:19:07 -0600

    class1

---
+++ program.cs
@@ -0,0 +1,21 @@
+
+using ConsoleApplication2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program {
+    public static void Main(string[] args) {
+        Class1 obj = new Class1();
+
+        obj.reptiles();
+        obj.aves();
+        obj.mamiferos();
+
+        Console.ReadKey();
+
+    }
+}
